56ae57ef0c965e5dddbad8f9d5807d36017a8aa1
M8MBA <sauce.ix@gmail.com>
2022-02-12 23:40:39 -0700

    password requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,29 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+const UNIlower = [97, 122];
+const UNIupper = [65, 90];
+const UNInumber = [48, 57];
+const UNIsymbols = [33,47];
 
 // Write password to the #password input
 function writePassword() {
+
+  var password = prompt("Please select password length")
+  if (password < 8 || password > 128) {
+    alert("Incorrect password length")
+    generatePassword()
+  } else if (password === '' || isNaN(password)) {
+    alert("Please enter a password length between 8 & 128")
+    generatePassword()
+  }
+
+  var lowercase = confirm("Would you like")
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
