4ee8ffaddfa96961b0de6feb9fa750d89f8298f5
M8MBA <sauce.ix@gmail.com>
2022-02-13 14:48:00 -0700

    generated success

---
+++ README.md
@@ -0,0 +1,38 @@
+# Password Generator
+
+# <img width="837" alt="PassGen-SS" src="https://user-images.githubusercontent.com/97080366/153775979-fbf94a7f-62fe-47f2-86eb-c1df9d39e64d.png">
+
+## Purpose
+
+Create an application that an employee can use to generate a random password based on following criteria:
+
+    GIVEN I need a new, secure password
+    WHEN I click the button to generate a password
+    THEN I am presented with a series of prompts for password criteria
+    WHEN prompted for password criteria
+    THEN I select which criteria to include in the password
+    WHEN prompted for the length of the password
+    THEN I choose a length of at least 8 characters and no more than 128 characters
+    WHEN asked for character types to include in the password
+    THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    WHEN I answer each prompt
+    THEN my input should be validated and at least one character type should be selected
+    WHEN all prompts are answered
+    THEN a password is generated that matches the selected criteria
+    WHEN the password is generated
+    THEN the password is either displayed in an alert or written to the page
+
+The app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code I have written. The app will have a clean polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+## Built With
+* HTML
+* CSS
+* JS
+
+## Website
+https://m8mba.github.io/password-generator/
+
+## Contribution
+Made with ❤️ by Cade Wilson
+
+### ©️2022 Cade Wilson
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,62 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-const UNIlower = [97, 122];
-const UNIupper = [65, 90];
-const UNInumber = [48, 57];
-const UNIsymbols = [33,47];
+const lowerCaseAZ = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+const upperCaseAZ = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const symbols = ["!", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-// Write password to the #password input
-function writePassword() {
+function generatePassword() {
+  let characterTypes = [];
 
-  var password = prompt("Please select password length")
+  var password = prompt("Please select password length between 8 & 128 characters")
   if (password < 8 || password > 128) {
     alert("Incorrect password length")
-    generatePassword()
+    generatePassword();
   } else if (password === '' || isNaN(password)) {
-    alert("Please enter a password length between 8 & 128")
-    generatePassword()
+    alert("Please enter a password length between 8 & 128 characters")
+    generatePassword();
   }
 
-  var lowercase = confirm("Would you like")
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  var lowerCase = confirm("Would you like to include lowercase letters in your password?");
+  var upperCase = confirm("Would you like to include uppercase letters in your password?");
+  var numeric = confirm("Would you like to include numbers in your password?");
+  var special = confirm("Would you like to include special characters in your password?"); 
 
+  if (lowerCase == true) {
+    characterTypes = characterTypes.concat(lowerCaseAZ);
+  }
+  if (upperCase == true) {
+    characterTypes = characterTypes.concat(upperCaseAZ);
+  }
+  if (numeric == true) {
+    characterTypes = characterTypes.concat(num);
+  }
+  if (special == true) {
+    characterTypes = characterTypes.concat(symbols);
+  }
+  if (lowerCase == false && upperCase == false && special == false && numeric == false) {
+    alert("Please select at least one option")
+  }
 
+  console.log(characterTypes);
+
+  var genPassword = ""
+
+  for (let i = 0, p = characterTypes.length; i < parseInt(password); i++) {
+    genPassword += characterTypes[Math.floor(Math.random() * p)]
+  }
+  return genPassword;
+};
+
+// Write password to the #password input
+function writePassword () {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
